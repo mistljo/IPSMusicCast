162579346df7b395a4371b30caf7baf8a9fb5039
mistljo <joachim@joko.at>
2018-12-10 19:57:17 +0100

    Beta
    first Version

--- IPSMusicCast/module.php
+++ IPSMusicCast/module.php
@@ -1,29 +1,160 @@
 <?
+define('__ROOT__', dirname(dirname(__FILE__)));
+require_once(__ROOT__ . '/libs/autoload.php');
 
-	class IPSMusicCast extends IPSModule
-	{
-
-		public function CallBoolean(bool $Value)
+class IPSMusicCast extends IPSModule
+{
+    public function Create()
+    {
+        parent::Create();
+		
+		//Globale Variablen Profile erstellen
+		if (!IPS_VariableProfileExists("MUC_Mute"))
 		{
-			if($Value) {
-				echo "True";
-			} else {
-				echo "False";
+		IPS_CreateVariableProfile("MUC_Mute", 0);
+		IPS_SetVariableProfileValues("MUC_Mute", 0, 1, 1);
+		IPS_SetVariableProfileAssociation("MUC_Mute", 0, "Unmute", "Speaker", -1);
+		IPS_SetVariableProfileAssociation("MUC_Mute", 1, "Mute", "Speaker", -1);
+		}
+		if (!IPS_VariableProfileExists("MUC_Volume"))
+		{
+		IPS_CreateVariableProfile("MUC_Volume", 1);
+		IPS_SetVariableProfileValues("MUC_Volume", 0, 60, 1);
+		IPS_SetVariableProfileIcon("MUC_Volume",  "Intensity");
+		IPS_SetVariableProfileText("MUC_Volume", "", "%");
+		}
+		if (!IPS_VariableProfileExists("MUC_State"))
+		{
+		IPS_CreateVariableProfile("MUC_State", 1);
+		IPS_SetVariableProfileAssociation("MUC_State", 201, "stop", "Speaker", -1);
+		IPS_SetVariableProfileAssociation("MUC_State", 202, "play", "Speaker", -1);
+		IPS_SetVariableProfileAssociation("MUC_State", 203, "pause", "Speaker", -1);
+		//IPS_SetVariableProfileAssociation("MUC_State", 204, "TRANSITIONING", "Speaker", -1);
+		//IPS_SetVariableProfileAssociation("MUC_State", 205, "UNKNOWN", "Speaker", -1);
+		}
+		if (!IPS_VariableProfileExists("MUC_PreviousNext"))
+		{
+		IPS_CreateVariableProfile("MUC_PreviousNext", 1);
+		IPS_SetVariableProfileValues("MUC_PreviousNext", 0, 1, 1);
+		IPS_SetVariableProfileAssociation("MUC_PreviousNext", 0, "previous", "Speaker", -1);
+		IPS_SetVariableProfileAssociation("MUC_PreviousNext", 1, "next", "Speaker", -1);
+		}
+    }
+
+	public function Destroy()
+		{
+		parent::Destroy();
+			//Globale Variablen Profile löschen
+			if (IPS_VariableProfileExists("MUC_Mute"))
+			{IPS_DeleteVariableProfile("MUC_Mute");}
+			if (IPS_VariableProfileExists("MUC_Volume"))
+			{IPS_DeleteVariableProfile("MUC_Volume");}
+			if (IPS_VariableProfileExists("MUC_State"))
+			{IPS_DeleteVariableProfile("MUC_State");}
+			if (IPS_VariableProfileExists("MUC_PreviousNext"))
+			{IPS_DeleteVariableProfile("MUC_PreviousNext");}
+
+		}
+//Modul gespeichert
+	public function ApplyChanges()
+		{
+			parent::ApplyChanges();
+
+			// UDP Socket erstellen oder verbinden
+			$this->ConnectParent("{82347F20-F541-41E1-AC5B-A636FD3AE2D8}");
+			$UDPSocket = $this->GetParentId(); //UDP Socket ID finden
+			//Properties für das UDP Socket einstellen
+			IPS_SetProperty($UDPSocket, "BindPort", 41100);
+			IPS_SetProperty($UDPSocket, "Host", "192.168.1.160");
+			IPS_SetProperty($UDPSocket, "Port", 41100);
+			IPS_SetProperty($UDPSocket, "Open", true);
+			IPS_ApplyChanges($UDPSocket);
+			//$this->SetStatus(104);
+		}
+
+	public function GetMCDevices()
+		{
+			//Delete Cache Folder
+			$tempPath = sys_get_temp_dir() . DIRECTORY_SEPARATOR . "musiccast";
+			$this->rrmdir($tempPath);
+			
+			//Get all Speakers in current Network
+			$musicCastNetwork = new MusicCast\Network;
+			try {
+				$MUCspeakers = $musicCastNetwork->getSpeakers();
+				}
+			catch (Exception $e) {
+
+				echo 'Error: ',  $e->getMessage(), "\n";
+				$this->SetStatus(104);
+				exit(1);
+			}		
+			$this->SetStatus(102);
+			$SpeakerIPs = array_keys($MUCspeakers);
+			
+			//Liest bestehende MusicCast Geräte aus ips aus
+			$MusicCastInstances = (IPS_GetInstanceListByModuleID('{332C2875-7503-4054-95BE-250081AF03DF}'));
+			$ExistingDevicesIDs = [];
+			foreach ($MusicCastInstances as $MusicCastInstance)
+			{
+				array_push($ExistingDevicesIDs, IPS_GetProperty($MusicCastInstance, "DeviceID"));
+			}
+
+			//Speaker Instancen erstellen
+			if (isset($MUCspeakers) && is_array($MUCspeakers)) {
+				foreach ($SpeakerIPs as $SpeakerIP)
+				{
+					$musicCastDevice = new MusicCast\Device($SpeakerIP);
+					$musicCastSpeaker = new MusicCast\Speaker($musicCastDevice);
+					$SpeakerName = $musicCastSpeaker->getName();
+					$SpeakerID = $musicCastSpeaker->getUuid();
+					//Prüfe ob Instance schon vorhanden
+						if (!in_array($SpeakerID, $ExistingDevicesIDs))
+						{
+							IPS_LogMessage("MusicCast", "Add new Speaker/Controller to system: " . $SpeakerName . " IP: " .$SpeakerIP);
+							
+							$NewMUCDevice = IPS_CreateInstance('{332C2875-7503-4054-95BE-250081AF03DF}');
+							IPS_SetName($NewMUCDevice,$SpeakerName);
+							IPS_SetProperty($NewMUCDevice, "DeviceID", $SpeakerID);
+							IPS_SetProperty($NewMUCDevice, "Host", $SpeakerIP);
+							IPS_SetProperty($NewMUCDevice, "Name", $SpeakerName);
+							IPS_ApplyChanges($NewMUCDevice);
+							//MUC_DeviceSetup($NewMUCDevice);
+							
+						} else {
+							IPS_LogMessage("MusicCast", "Speaker/Controller " . $SpeakerName . " already exist. -> skip");
+						}
+					}
+				echo "Done, see Message Log for more Infos";
+			}
+			
+		}
+	
+    /**
+     * get connected parent instance id
+     * @return mixed
+     */
+    protected function GetParentId()
+    {
+        $instance = @IPS_GetInstance($this->InstanceID);
+        return $instance['ConnectionID'];
+    }
+
+	protected function rrmdir($src) {
+		$dir = opendir($src);
+		while(false !== ( $file = readdir($dir)) ) {
+			if (( $file != '.' ) && ( $file != '..' )) {
+				$full = $src . '/' . $file;
+				if ( is_dir($full) ) {
+					$this->rrmdir($full);
+				}
+				else {
+					unlink($full);
+				}
 			}
 		}
-
-		public function CallInteger(int $Value) {
-			echo $Value;
-		}
-
-		public function CallFloat(float $Value) {
-			echo $Value;
-		}
-
-		public function CallString(string $Value) {
-			echo $Value;
-		}
-
+		closedir($dir);
+		rmdir($src);
 	}
-
+}
 ?>
---
+++ IPSMusicCastDevice/module.php
@@ -0,0 +1,409 @@
+<?php
+define('__ROOT__', dirname(dirname(__FILE__)));
+require_once(__ROOT__ . '/libs/autoload.php');
+
+class IPSMusicCastDevice extends IPSModule
+{
+    public function Create()
+    {
+        parent::Create();
+		$this->RegisterPropertyString('DeviceID', ''); //Device ID
+		$this->RegisterPropertyString('Host', ''); //Device IP
+		$this->RegisterPropertyString('Name', ''); //Device Name
+
+		$this->RegisterVariableBoolean("Power", "Power");
+		IPS_SetVariableCustomProfile($this->GetIDForIdent("Power"), "~Switch");
+		IPS_SetPosition($this->GetIDForIdent("Power"), 0);
+		$this->EnableAction("Power");
+		
+		$this->RegisterVariableInteger("State", "State");
+		IPS_SetVariableCustomProfile($this->GetIDForIdent("State"), "MUC_State");
+		IPS_SetPosition($this->GetIDForIdent("State"), 1);
+		$this->EnableAction("State");
+		
+		$this->RegisterVariableInteger("PreviousNext", "PreviousNext");
+		IPS_SetVariableCustomProfile($this->GetIDForIdent("PreviousNext"), "MUC_PreviousNext");
+		IPS_SetPosition($this->GetIDForIdent("PreviousNext"), 2);
+		$this->EnableAction("PreviousNext");
+		
+		$this->RegisterVariableInteger("Volume", "Volume");
+		IPS_SetPosition($this->GetIDForIdent("Volume"), 4);
+		IPS_SetVariableCustomProfile($this->GetIDForIdent("Volume"), "MUC_Volume");
+		$this->EnableAction("Volume");
+		
+		$this->RegisterVariableBoolean("Mute", "Mute");
+		IPS_SetVariableCustomProfile($this->GetIDForIdent("Mute"), "MUC_Mute");
+		IPS_SetPosition($this->GetIDForIdent("Mute"), 5);
+		$this->EnableAction("Mute");
+
+		$this->RegisterVariableInteger("Input", "Input");
+		IPS_SetPosition($this->GetIDForIdent("Input"), 6);
+		$this->EnableAction("Input");
+		
+		$this->RegisterVariableString("Playtime", "Play Time");
+		IPS_SetPosition($this->GetIDForIdent("Playtime"), 7);
+		
+		$this->RegisterVariableString("Title", "Title");
+		IPS_SetPosition($this->GetIDForIdent("Title"), 8);
+		
+		$this->RegisterVariableString("Artist", "Artist");
+		IPS_SetPosition($this->GetIDForIdent("Artist"), 9);
+		
+		$this->RegisterVariableString("Album", "Album");
+		IPS_SetPosition($this->GetIDForIdent("Album"), 10);
+		
+		$this->RegisterVariableString("AlbumArt", "Cover");
+		IPS_SetPosition($this->GetIDForIdent("AlbumArt"), 11);
+		IPS_SetVariableCustomProfile($this->GetIDForIdent("AlbumArt"), "~HTMLBox");
+
+		// register update timer for Device subscription
+		$this->RegisterTimer('subscribeDevicesTimer', 2000, 'MUC_subscribeDevice($_IPS[\'TARGET\']);');
+
+		// register onetime-timer for Device Setup
+		$this->RegisterTimer('setupOneTimeTimer', 2000, 'MUC_DeviceSetup($_IPS[\'TARGET\']);');
+    }
+
+    public function Destroy()
+    {
+        parent::Destroy();
+		//Funktioniert nicht, warum?
+		
+		/*if (IPS_VariableProfileExists("MUC_Input_" . $this->ReadPropertyString('DeviceID')))
+		{
+			IPS_DeleteVariableProfile("MUC_Input_" . $this->ReadPropertyString('DeviceID'));
+		}*/
+    }
+//Modul gespeichert
+	    public function ApplyChanges()
+    {
+        parent::ApplyChanges();
+		$this->ConnectParent("{82347F20-F541-41E1-AC5B-A636FD3AE2D8}");
+    }
+
+public function subscribeDevice()
+	{
+		IPS_LogMessage("MUC ". $this->ReadPropertyString('Name'), "Subscribe Device: " . $this->ReadPropertyString('Host'));
+		$DeviceHost = $this->ReadPropertyString('Host');
+		$musicCastClient = new MusicCast\Client(['host' => $DeviceHost,'port' => 80,]);
+		$result = $musicCastClient->api('events')->subscribe();
+		$timer = 540000; //9 Minuten
+		$this->SetTimerInterval('subscribeDevicesTimer', $timer);
+	}
+
+public function updateSpeakerIP()
+{
+		//Update device IP
+		$tempPath = sys_get_temp_dir() . DIRECTORY_SEPARATOR . "musiccast";
+		$this->rrmdir($tempPath);
+		$CurrentIP = $this->getSpeakerIPbyName($this->ReadPropertyString('Name'));
+		if($CurrentIP != $this->ReadPropertyString('Host'))
+			{
+				IPS_LogMessage("MUC " . $this->ReadPropertyString('Name'), "Device IP updated");
+				SetValue($this->GetIDForIdent('Input'),$CurrentIP);
+			} else {
+				IPS_LogMessage("MUC " . $this->ReadPropertyString('Name'), "Device IP is still valid");
+			}
+		$this->SetStatus(102);
+}
+	//Get Data from UDP Socket
+		public function ReceiveData($JSONString)
+		{
+			$data = json_decode($JSONString);
+			//Parse and write values to our buffer
+			$this->SetBuffer("MusicCastDeviceBuffer", utf8_decode($data->Buffer));
+			$MUC_Buffer = $this->GetBuffer("MusicCastDeviceBuffer");
+			$MUC_Buffer_JSON = json_decode($MUC_Buffer,false);
+			//print_r($MUC_Buffer_JSON);
+			if ($MUC_Buffer_JSON->device_id == $this->ReadPropertyString('DeviceID'))
+			{
+				if (key($MUC_Buffer_JSON) == 'main') {
+					if (property_exists($MUC_Buffer_JSON->main, 'mute')) {
+							IPS_LogMessage("MUC " . $this->ReadPropertyString('Name') . " [Main]", $MUC_Buffer_JSON->main->mute);
+							$this->SetValue("Mute", $MUC_Buffer_JSON->main->mute);
+						}
+					if (property_exists($MUC_Buffer_JSON->main, 'volume')) {
+							IPS_LogMessage("MUC " . $this->ReadPropertyString('Name') . " [Main] Volume", $MUC_Buffer_JSON->main->volume);
+							$this->SetValue("Volume", $MUC_Buffer_JSON->main->volume);
+						}
+					if (property_exists($MUC_Buffer_JSON->main, 'input')) {
+							IPS_LogMessage("MUC " . $this->ReadPropertyString('Name') . " [Main] Input", $MUC_Buffer_JSON->main->input);
+							SetValue($this->GetIDForIdent('Input'), $this->getVariableIntegerbyName($this->GetIDForIdent('Input'),$MUC_Buffer_JSON->main->input));
+						}
+					if (property_exists($MUC_Buffer_JSON->main, 'signal_info_updated')) {
+							IPS_LogMessage("MUC " . $this->ReadPropertyString('Name') . " [Main]->signal_info_updated", $MUC_Buffer_JSON->main->signal_info_updated);
+							$this->updateSpeakerInfos();
+						}	
+					if (property_exists($MUC_Buffer_JSON->main, 'power')) {
+							IPS_LogMessage("MUC " . $this->ReadPropertyString('Name') . " [Main]->power", $MUC_Buffer_JSON->main->power);
+							if ($MUC_Buffer_JSON->main->power == 'on')
+							{
+								SetValue($this->GetIDForIdent('Power'),true);
+								$this->setHiddenDeviceVariable($this->GetIDForIdent('Power'),false);
+							}else
+							{
+								SetValue($this->GetIDForIdent('Power'),false);
+								$this->setHiddenDeviceVariable($this->GetIDForIdent('Power'),true);
+							}
+						}
+				}
+				if (key($MUC_Buffer_JSON) == 'netusb')
+				{
+					
+					if (property_exists($MUC_Buffer_JSON->netusb, 'play_time')) {
+						$playtime = gmdate("H:i:s", $MUC_Buffer_JSON->netusb->play_time);
+						$this->SetValue("Playtime", $playtime);
+					}
+					if (property_exists($MUC_Buffer_JSON->netusb, 'signal_info_updated')) {
+						IPS_LogMessage("MUC " . $this->ReadPropertyString('Name') . " [netusb]->signal_info_updated", $MUC_Buffer_JSON->netusb->signal_info_updated);
+					}
+					if (property_exists($MUC_Buffer_JSON->netusb, 'play_info_updated')) {
+						IPS_LogMessage("MUC " . $this->ReadPropertyString('Name') . " [netusb]->player_info_updated", $MUC_Buffer_JSON->netusb->play_info_updated);
+						$this->updateSpeakerInfos();
+					}
+				}
+			}
+		}
+		
+
+public function updateSpeakerInfos()
+	{
+		$musicCastNetwork = new MusicCast\Network;
+		$DeviceHost = $this->ReadPropertyString('Host');
+		$musicCastDevice = new MusicCast\Device($DeviceHost);
+		$musicCastSpeaker = new MusicCast\Speaker($musicCastDevice);
+		$musicCastController = new MusicCast\Controller($musicCastSpeaker,$musicCastNetwork,1);
+		//Update Album Details
+		$StateDetails = $musicCastController->getStateDetails();
+		$this->SetValue("Title", $this->getObjProp($StateDetails->track,"title"));
+		$this->SetValue("Artist", $this->getObjProp($StateDetails->track,"artist"));
+		$this->SetValue("Album", $this->getObjProp($StateDetails->track,"album"));
+		if ($this->getObjProp($StateDetails->track,"albumArt") == "")
+		{
+			IPS_SetHidden($this->GetIDForIdent('AlbumArt'),true);
+		}else{
+			$AlbumArtHTML = "<img src='" . $this->getObjProp($StateDetails->track,"albumArt") . "' style='height: 20%; width: 20%; object-fit: fill;' />";
+			$this->SetValue("AlbumArt", $AlbumArtHTML);
+			IPS_SetHidden($this->GetIDForIdent('AlbumArt'),false);
+		}
+		//Update State (play,stop,pause)
+		$this->SetValue("State", $musicCastController->getState());
+		//Update Power 
+		$powerstate = $musicCastController->isPowerOn();
+			if($powerstate == false)
+			{
+				$this->SetValue("Power", $powerstate);
+				$this->setHiddenDeviceVariable($this->GetIDForIdent("Power"),true);
+				//$this->SetTimerInterval('subscribeDevicesTimer', 0);
+			}else{
+				$this->SetValue("Power", $powerstate);
+				$this->setHiddenDeviceVariable($this->GetIDForIdent("Power"),false);
+			}
+		
+		//Update Volume 
+		$this->SetValue("Volume", $musicCastController->getVolume());
+		//Update Mute 
+		$this->SetValue("Mute", $musicCastController->isMuted());
+		//Update Input
+		$CurrentInput = $musicCastController->getInput();
+		$this->SetValue("Input", $this->getVariableIntegerbyName($this->GetIDForIdent('Input'),$CurrentInput));
+	}
+
+//function extract protected properties
+protected function getObjProp($obj, $val){
+	$propGetter = Closure::bind( function($prop){return $this->$prop;}, $obj, $obj );
+	return $propGetter($val);
+}
+
+public function RequestAction($Ident, $Value) {
+ 		$musicCastNetwork = new MusicCast\Network;
+		$DeviceHost = $this->ReadPropertyString('Host');
+		$musicCastDevice = new MusicCast\Device($DeviceHost);
+		$musicCastSpeaker = new MusicCast\Speaker($musicCastDevice);
+		$musicCastController = new MusicCast\Controller($musicCastSpeaker,$musicCastNetwork,1);
+
+
+    switch($Ident) {
+        case "Mute":
+			if($Value == true)
+			{
+				$result = $musicCastController->mute();
+			}else{
+				$result = $musicCastController->unmute();
+			}
+            break;
+        case "Power":
+			if($Value == false)
+			{
+				$result = $musicCastController->standBy();
+				$this->setHiddenDeviceVariable($this->GetIDForIdent($Ident),true);
+			}else{
+				$result = $musicCastController->powerOn();
+				$this->setHiddenDeviceVariable($this->GetIDForIdent($Ident),false);
+				$this->subscribeDevice();
+				$this->updateSpeakerInfos();
+			}
+            break;
+        case "Volume":
+			$result = $musicCastController->setVolume($Value);
+            break;
+        case "State":
+			$result = $musicCastController->setState($Value);
+            break;
+        case "Input":
+			$InputName = $this->getVariableValueName($this->GetIDForIdent($Ident),$Value);
+			$result = $musicCastController->setInput($InputName);
+            break;
+        case "PreviousNext":
+			if($Value == false)
+			{$result = $musicCastController->previous();}
+			else
+			{$result = $musicCastController->next();}
+			
+            break;
+        default:
+            throw new Exception("Invalid Ident");
+    }
+}
+//Versteckt alle Objekte wenn Power vom Device off ist
+protected function setHiddenDeviceVariable($PowerChildrenID,$hide)
+{
+	$ParentID = IPS_GetParent($PowerChildrenID);
+	$ChildrenIDs = IPS_GetChildrenIDs($ParentID);
+	foreach ($ChildrenIDs as $ChildrenID)
+	{
+		if(IPS_GetName($ChildrenID) != "Power")
+		{
+		IPS_SetHidden($ChildrenID,$hide);
+		}
+	}
+}
+
+//Wird ausgefürt nachdem die Instanze angelegt wurde (durch Timer)
+public function DeviceSetup()
+{
+	$this->SetTimerInterval('setupOneTimeTimer', 0);
+	$this->createInputVariablenprofile();
+	IPS_SetVariableCustomProfile($this->GetIDForIdent("Input"), "MUC_Input_" . $this->ReadPropertyString('DeviceID'));
+	$this->updateSpeakerIP();
+	$this->updateSpeakerInfos();
+}
+protected function getVariableValueName($VariableID,$VariableValue)
+{
+	$VariableObject = IPS_GetVariable($VariableID);
+	$VariableCustomProfileName = $VariableObject['VariableCustomProfile'];
+	$VariableProfileObject = IPS_GetVariableProfile($VariableCustomProfileName);
+	$VariableProfileValueName = $VariableProfileObject['Associations'][$VariableValue]['Name'];
+	return $VariableProfileValueName;
+}
+
+protected function getVariableIntegerbyName($VariableID,$VariableValueName)
+{
+	$i=0;
+	$VariableObject = IPS_GetVariable($VariableID);
+	$VariableCustomProfileName = $VariableObject['VariableCustomProfile'];
+	$VariableProfileObject = IPS_GetVariableProfile($VariableCustomProfileName);
+	foreach($VariableProfileObject['Associations'] as $VariableProfileObjectSub)
+	{
+		if ($VariableProfileObjectSub['Name'] == $VariableValueName)
+		{
+			$VariableProfileValueInt = $i;
+		}
+		$i++;
+	}
+	
+	return $VariableProfileValueInt;
+}
+
+protected function getSpeakerIPbyName($SpeakerName)
+{
+		$musicCastNetwork = new MusicCast\Network;
+		try {
+				$speaker = $musicCastNetwork->getSpeakerByName($SpeakerName);
+				$DeviceObj = $this->getObjProp($speaker,"device");
+				$DeviceIP = $this->getObjProp($DeviceObj,"ip");
+				return $DeviceIP;
+			}
+		catch (Exception $e) {
+				$this->SetStatus(104);
+				echo 'Error: ',  $e->getMessage(), "\n";
+				exit(1);
+		}
+}
+
+
+/*
+public function getNetPreset(){
+			$Data = "netusb/getPresetInfo"; 
+			$Answer = $this->SendCommand($Data);
+			$NetPresets = json_decode($Answer,false);
+			return ($NetPresets); 
+			}
+			*/
+			
+			
+//Input Variable Profile create
+protected function createInputVariablenprofile()
+	{
+		$DeviceID = $this->ReadPropertyString('DeviceID');
+		$musicCastNetwork = new MusicCast\Network;
+		$DeviceHost = $this->ReadPropertyString('Host');
+		$musicCastDevice = new MusicCast\Device($DeviceHost);
+		$musicCastSpeaker = new MusicCast\Speaker($musicCastDevice);
+		$musicCastController = new MusicCast\Controller($musicCastSpeaker,$musicCastNetwork,1);
+		$Inputs = $musicCastController->getInputList();
+		
+		$VariablenProfileName = "MUC_Input_" . $DeviceID;
+		if (!IPS_VariableProfileExists($VariablenProfileName)) 
+		{
+			IPS_CreateVariableProfile($VariablenProfileName, 1);
+			$i = 0;
+			foreach ($Inputs as $Input){
+				IPS_SetVariableProfileAssociation($VariablenProfileName, $i, $Input, "Speaker",-1);
+				$i++;
+			}
+		}
+	}
+	
+	protected function getInstanceNameExist($InstanceName)
+		{
+			$Instanceexist = false;
+			$Instances = IPS_GetInstanceList();
+			foreach($Instances as $Instance)
+			{
+				$InstanceObject = IPS_GetInstance($Instance);
+				$InstanceObjectName = IPS_GetName($InstanceObject['InstanceID']);
+				
+				if ($InstanceObjectName == $InstanceName) $Instanceexist = true;
+			}
+			return $Instanceexist;
+		}
+    /**
+     * reconnect parent socket
+     * @param bool $force
+     */
+    public function ReconnectParentSocket($force = false)
+    {
+        $ParentID = $this->GetParentId();
+        if (($this->HasActiveParent() || $force) && $ParentID > 0) {
+            IPS_SetProperty($ParentID, 'Open', true);
+            @IPS_ApplyChanges($ParentID);
+        }
+    }
+
+	protected function rrmdir($src) {
+		$dir = opendir($src);
+		while(false !== ( $file = readdir($dir)) ) {
+			if (( $file != '.' ) && ( $file != '..' )) {
+				$full = $src . '/' . $file;
+				if ( is_dir($full) ) {
+					$this->rrmdir($full);
+				}
+				else {
+					unlink($full);
+				}
+			}
+		}
+		closedir($dir);
+		rmdir($src);
+	}
+}
